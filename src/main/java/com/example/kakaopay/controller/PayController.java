7b285524c5f7ea5484971e5822706151f7878bd6
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-17 01:42:27 +0900

    finish

--- src/main/java/com/example/kakaopay/controller/PayController.java
+++ src/main/java/com/example/kakaopay/controller/PayController.java
@@ -1,16 +1,38 @@
 package com.example.kakaopay.controller;
 
+import com.example.kakaopay.vo.RequestPay;
+import com.example.kakaopay.vo.ResponsePay;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.Thymeleaf;
 
-@Controller
+@RestController
 @Log4j2
 public class PayController {
 
-    @GetMapping("/pay")
-    public void pay() {
+    @RequestMapping("/pay")
+    public ModelAndView pay() {
         log.info("pay");
+        return new ModelAndView("pay");
+    }
+
+    @RequestMapping("/pay/completed")
+    public ModelAndView completed() {
+        log.info("pay");
+        return new ModelAndView("completed");
+    }
+
+    @PostMapping("/payments/complete")
+    public HttpStatus complete(@RequestBody Object object) {
+        System.out.println(object);
+        log.info("--------------------------------------------------------------");
+        log.info(object);
+        return HttpStatus.OK;
     }
 
 }
